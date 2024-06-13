dcfa2472a6ced08f789eb3fa4ea15debe0902f8b
abzalle <145341019+abzalle@users.noreply.github.com>
2024-06-13 13:53:43 +0500

    Delete практика сайт2 directory

--- практика сайт2/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // инициализация слайдера
-    var slider = new SimpleAdaptiveSlider('.slider', {
-      autoplay: true,
-      interval: 10000,
-    });
-  });
